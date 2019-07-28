fdce950c642785e2354a516dde3bc52da464e20d
Elias Rodriguez <elias.rod@live.com>
2019-07-27 21:31:11 -0300

    Translator version

--- myFirstAzureFunction.cs
+++ myFirstAzureFunction.cs
@@ -7,8 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
 
-namespace test
+namespace azureFunctionsWithDocker
 {
     public static class myFirstAzureFunction
     {
@@ -17,17 +18,23 @@ namespace test
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request. Translates any text to english");
 
-            string name = req.Query["name"];
-
+            string sourceText = req.Query["sourceText"];
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            sourceText = sourceText ?? data?.sourceText;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            string uri = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190727T200316Z.786cd4031b1af802.03ef3a949ee8e7d5606311b0055193ffaee4d6cf&lang=en&text=" + sourceText;
+            HttpClient client = new HttpClient();
+            string responseBody = await client.GetStringAsync(uri);
+            dynamic responseBodySerialized = JsonConvert.DeserializeObject(responseBody);
+            string targetText = responseBodySerialized?.text[0];
+
+            return sourceText != null && targetText != null
+                ? (ActionResult)new OkObjectResult("\"" + sourceText + "\"" + " translated to English: " + targetText)
+                : new BadRequestObjectResult("Please pass a text on the query string or in the request body");
         }
     }
 }
